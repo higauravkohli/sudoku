db40d951bdb1cb0d86e8b8e248839ccc030d9914
g <g@ubuntu>
2018-07-07 23:18:56 -0700

    basic impl

--- sudoku/src/main/java/com/gk/sudoku/algo/SudokuSolver.java
+++ sudoku/src/main/java/com/gk/sudoku/algo/SudokuSolver.java
@@ -1,9 +1,8 @@
 package com.gk.sudoku.algo;
 
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.Collection;
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.stereotype.Component;
 
@@ -13,48 +12,124 @@ import com.gk.sudoku.model.Sudoku;
 public class SudokuSolver{
 
 	public Sudoku solve(Sudoku sudoku) {
+		boolean matched = false;
+		do {
+			matched = false;
+			
+			int[][][] possibilities = possibilities(sudoku);
+			if(confirmedHit(sudoku, possibilities))
+				matched = true;
+			
+		}while(matched);
+		
 		return sudoku;
 	}
 
-	private void confirmedHit(Sudoku sudoku) {
-		int[] next = next(sudoku);
-		
-		if(next != null) {
-			for(int i=0; i<9; i++) {
-				rowPossibilities(sudoku, next, i+1);
+	private int[][][] possibilities(Sudoku sudoku){
+		int[][] puzzle = sudoku.getPuzzle();
+		int[][][] possibilities = new int[9][9][9];
+		for(int x=0; x<9; x++) {
+			for(int y=0; y<9; y++) {
+				if(puzzle[x][y] == 0) {
+					List<Integer> possibilitySet = new ArrayList<>();
+					
+					List<Integer> rows = rowPossibilities(sudoku, y);
+					List<Integer> cols = colPossibilities(sudoku, x);
+					List<Integer> boxes = boxPossibilities(sudoku, x, y);
+					
+					for(Integer val : rows) {
+						if(cols.contains(val) && boxes.contains(val))
+							possibilitySet.add(val);
+					}
+					
+					possibilities[x][y] = listToArray(possibilitySet);
+				}
 			}
 		}
+		return possibilities;
 	}
 	
-	private int[] rowPossibilities(Sudoku sudoku, int[] xy, int val){
-		List<Integer> possibilities = new ArrayList<>();
-		for(int y=0; y<9; y++) {
-			if(y==0)
-				possibilities.add(y);
-			if(sudoku.get(xy[0], y) == val)
-				return null;
-		}
-
-		return listToArray(possibilities);
+	private boolean confirmedHit(Sudoku sudoku, int[][][] possibilities) {
+		return singleHit(sudoku, possibilities);
 	}
 
-	private int[] listToArray(List<Integer> values) {
+	private boolean singleHit(Sudoku sudoku, int[][][] possibilities) {
+		boolean hit = false;
+		for(int x=0; x<9; x++) {
+			for(int y=0; y<9; y++) {
+				if(possibilities[x][y].length == 1) {
+					sudoku.set(x, y, possibilities[x][y][0]);
+					hit = true;
+				}
+			}
+		}
+		return hit;
+	}
+	
+	private List<Integer> rowPossibilities(Sudoku sudoku, int y){
+		List<Integer> possibilities = new ArrayList<>();
+		for(int i=0; i<9; i++) {
+			boolean found = false;
+			for(int x=0; x<9; x++) {
+				if(sudoku.get(x, y) == (i+1)) {
+					found = true;
+					break;
+				}
+			}
+			if(!found)
+				possibilities.add(i+1);
+		}
+		
+		return possibilities;
+	}
+
+	private List<Integer> colPossibilities(Sudoku sudoku, int x){
+		List<Integer> possibilities = new ArrayList<>();
+		for(int i=0; i<9; i++) {
+			boolean found = false;
+			for(int y=0; y<9; y++) {
+				if(sudoku.get(x, y) == (i+1)) {
+					found = true;
+					break;
+				}
+			}
+			if(!found)
+				possibilities.add(i+1);
+		}
+		
+		return possibilities;
+	}
+	
+	private List<Integer> boxPossibilities(Sudoku sudoku, int x, int y){
+		List<Integer> possibilities = new ArrayList<>();
+		for(int i=0; i<9; i++) {
+			boolean found = false;
+			for(int x1=0; x1<9; x1++) {
+				if(x1/3 == x/3) {
+					for(int y1=0; y1<9; y1++) {
+						if(y1/3 == y/3) {
+							if(sudoku.get(x1, y1) == (i+1)) {
+								found = true;
+								break;
+							}
+						}
+					}
+				}
+			}
+			if(!found)
+				possibilities.add(i+1);
+		}
+		
+		return possibilities;
+	}
+
+	private int[] listToArray(Collection<Integer> values) {
 		int[] valuesArr = new int[values.size()];
-		for(int i=0; i<values.size(); i++)
-			valuesArr[i] = values.get(i);
+		int i=0;
+		for(Integer vl : values)
+			valuesArr[i++] = vl;
 		
 		return valuesArr;
 	}
 	
-	private int[] next(Sudoku sudoku) {
-		int[][] puzzle = sudoku.getPuzzle();
-		for(int x=0; x<9; x++) {
-			for(int y=0; y<9; y++) {
-				if(puzzle[x][y] == 0) {
-					return new int[] {x,y};
-				}
-			}
-		}
-		return null;
-	}
 }
--- sudoku/src/main/java/com/gk/sudoku/model/Sudoku.java
+++ sudoku/src/main/java/com/gk/sudoku/model/Sudoku.java
@@ -17,6 +17,26 @@ public class Sudoku {
 	}
 	
 	public void set(int x, int y, int val) {
+		if(!fit(x, y, val))
+			throw new RuntimeException("Doesnt fit" + x + " " + y + " " + val);
 		puzzle[x][y] = val;
+	}
+	
+	public boolean fit(int x, int y, int val) {
+		return puzzle[x][y] == 0 && rowFit(x, val) && colFit(y, val);
+	}
+	
+	private boolean rowFit(int x, int val) {
+		for(int y=0; y<9; y++)
+			if(puzzle[x][y] == val)
+				return false;
+		return true;
+	}
+
+	private boolean colFit(int y, int val) {
+		for(int x=0; x<9; x++)
+			if(puzzle[x][y] == val)
+				return false;
+		return true;
 	}
 }
